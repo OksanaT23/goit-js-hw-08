f27c58d82c65e0267b373399d83d410689ba471f
OksanaT23 <129532941+OksanaT23@users.noreply.github.com>
2023-09-10 20:24:19 +0300

    Add lodash.throttle to hw 8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,17 @@
 import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 const storageKey = 'videoplayer-current-time';
 const currentTime = localStorage.getItem(storageKey);
 
-player.on('timeupdate', function (data) {
+const vimeoTimeUpdateCallback = function (data) {
     console.log(data);
     localStorage.setItem(storageKey, data.seconds);
-});
+};
+
+player.on('timeupdate', throttle(vimeoTimeUpdateCallback, 1000, {leading: true, trailing: false}));
 
 if (currentTime) {
     player.setCurrentTime(currentTime);
