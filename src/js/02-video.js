e6765042e9b8df253bf950609676c7a897bd887a
OksanaT23 <129532941+OksanaT23@users.noreply.github.com>
2023-09-10 20:13:22 +0300

    hw 8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const storageKey = 'videoplayer-current-time';
+const currentTime = localStorage.getItem(storageKey);
+
+player.on('timeupdate', function (data) {
+    console.log(data);
+    localStorage.setItem(storageKey, data.seconds);
+});
+
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
