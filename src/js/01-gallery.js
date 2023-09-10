fc1c69f63dfba3d2e7c0f22636157be43aef91a4
OksanaT23 <129532941+OksanaT23@users.noreply.github.com>
2023-09-10 19:39:10 +0300

    hw 8.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryCallback = ({ preview, original, description }) => {
+    return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+};
+
+const galleryElement = document.querySelector('ul.gallery');
+const galleryHtml = galleryItems.map(galleryCallback).join('');
+
+galleryElement.insertAdjacentHTML('beforeend', galleryHtml);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
